831fbd57013adf927615c2a4495dd2b1427a8646
Jiemin <wangjm323@gmail.com>
2022-06-17 15:12:35 -0400

    add some tests

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Error("Expected deck length of 20, but got ", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Error("Expected first card of Ace of Spades, but got ", d[0])
+	}
+
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Error("Expected 16 cards in deck, got ", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
